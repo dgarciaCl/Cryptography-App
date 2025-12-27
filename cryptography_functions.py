2f5ee276508ffab6f21931aa90cff81648af6226
100551049-ctrl <100551059@alumnos.uc3m.es>
2025-12-27 11:19:38 +0100

    signature edits

--- cryptography_functions.py
+++ cryptography_functions.py
@@ -7,6 +7,12 @@ from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
+
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+import pem
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+
 #REGISTRATION --------------
 def load_users(usersfile): #read the existing users
     if not os.path.exists(usersfile):
@@ -49,17 +55,38 @@ def chachapoly_decrypt(key, ciphertext, nonce):
 
 #SIGNATURE --------------
 
+#Serialise & store keys
+def serialise_private(key, user, pwd):
+    pem = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.BestAvailableEncryption(pwd)
+    )
+    filename = user + 'private' + '.pem'
+    with open(filename, "wb") as f:
+        f.write(pem)        #assume this is very secure and inaccessible by anyone
+def serialise_public(key, user):
+    pem = key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    filename = user + 'public' + '.pem'
+    with open(filename, "wb") as f:
+        f.write(pem)        #assume this is very secure and inaccessible by anyone
+
 #Signature of a file
-def sign(user, room, time):
-    #clear is a dictionary of the form {Name: Room, Time} in cleartext
+def sign(user, room, time, pwd):
     msg = user + room + time
     msg_byte = msg.encode("utf-8")
     json_name = msg + ".json"
-    private_key = rsa.generate_private_key(
+    private_key_byte = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
     )
-    signature = private_key.sign(
+    serialise_private(private_key_byte, 'private', msg, pwd)
+    public_key_byte = private_key_byte.public_key()
+    serialise_public(public_key_byte, 'public', msg)
+    signature = private_key_byte.sign(
         msg_byte,
         padding.PSS(
             mgf=padding.MGF1(hashes.SHA256()),
@@ -67,9 +94,31 @@ def sign(user, room, time):
         ),
         hashes.SHA256()
     )
-    data = {msg:signature}
+    signature_hex = signature.hex()
+    data = {msg:signature_hex}   
     with open(json_name, "w") as json_file: 
         json.dump(data, json_file, indent=4) #Adds the dictionaries' contents in the json  
+
+#verify the signature
+def verify_sign(json_file, pem_puk):
+    info = load_users(json_file)
+    message = info.keys()
+    message_byte = message.encode("utf-8")
+    signature_hex = info
+    signature_byte = bytes.fromhex(signature_hex)
+    with open(pem_puk, 'rb') as f:
+        public_key = serialization.load_pem_public_key(f.read())
+    public_key.verify(
+        signature_byte,
+        message_byte,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH,
+        ),
+        hashes.SHA256(),
+    )
+    print("Signature is valid")
+  
 
 #CREATE FILE ------------
 def create_user_file(username, room, roomnonce,  time, timenonce):
